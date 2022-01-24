b0ada4751ddf077d0d3fa9c5949df01492a90aff
Eduardo <96.rodedd@gmail.com>
2022-01-24 13:52:35 -0600

    menu mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Link } from 'react-router-dom';
 
+import { Menu } from './Menu';
+
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const openMenu = () => setIsOpen(!isOpen);
+
+
   return (
     <header className='flex w-full h-20'>
-      <nav className='flex justify-between items-center w-screen'>
+      <nav className='flex justify-between items-center w-screen relative'>
         <div>
           <Link to="/"> 
             <img className='w-28 rounded-full' src="" alt="Eduardo Prado" />
@@ -34,11 +40,16 @@ const Header = () => {
           </li>
         </ul>
 
-        <div className='w-12 h-12 flex justify-center items-center bg-gray-200 rounded-full cursor-pointer md:hidden'>
+        <div 
+          className='w-12 h-12 flex justify-center items-center bg-gray-200 rounded-full cursor-pointer md:hidden'
+          onClick={openMenu}
+        >
           <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
           </svg>
         </div>
+
+        {isOpen ? <Menu openMenu={openMenu} /> : null}
       </nav>
     </header>
   );
