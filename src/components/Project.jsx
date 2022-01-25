c61b1e10a6971054e9db318b1032cd5134b5b223
Eduardo <96.rodedd@gmail.com>
2022-01-24 19:17:05 -0600

    projects array

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,28 +1,31 @@
 import React from 'react';
 
-const Project = () => {
+const Project = ({ name, description, tags, liveUrl, githubUrl, image }) => {
   return (
     <div className='mb-12'>
       <div className='w-full'>
-        <img src="https://anniebombanie.com/assets/work-personal-date-night.png" alt="" />
+        <img src={image} alt="" />
       </div>
       
-      <h3 className='text-xl mb-4'>Project Title</h3>
-      <p className='text-lg mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quaerat error fugiat aliquid eos doloremque minima repellendus similique neque corrupti!</p>
+      <h3 className='text-xl mb-4'>{name}</h3>
+      <p className='text-lg mb-4'>{description}</p>
 
       <span className='flex items-center mb-4'>
         <svg className="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z" /></svg>
         <ul className='flex'>
-          <li>React.js</li>
-          <li>TailwindCSS</li>
+          {
+            tags.map(tag => (
+              <li>{tag}</li>
+            ))
+          }
         </ul>
       </span>
 
       <div>
-        <a className='bg-primary text-sm py-3 px-2 rounded-full inline-block w-24 text-center mr-4' href='https://www.github.com' target='_blank' rel='noreferrer'>
+        <a className='bg-primary text-sm py-3 px-2 rounded-full inline-block w-24 text-center mr-4' href={liveUrl} target='_blank' rel='noreferrer'>
           Live Demo
         </a>
-        <a className='bg-primary text-sm py-3 px-2 rounded-full inline-block w-32 text-center mr-10' href='https://www.github.com' target='_blank' rel='noreferrer'>
+        <a className='bg-primary text-sm py-3 px-2 rounded-full inline-block w-32 text-center mr-10' href={githubUrl} target='_blank' rel='noreferrer'>
           View on GitHub
         </a>
       </div>
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -2,6 +2,25 @@ import React from 'react';
 import { Project } from '../components/Project';
 import { Container } from '../layouts/Container';
 
+const projects = [
+  {
+    name: 'Projecto X',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quaerat error fugiat aliquid eos doloremque minima repellendus similique neque corrupti!',
+    tags: ['React.js', 'tailwind'],
+    liveUrl: 'https://github.com',
+    githubUrl: 'https://google.com',
+    image: 'https://anniebombanie.com/assets/work-personal-date-night.png',
+  },
+  {
+    name: 'Projecto 12312',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quaerat error fugiat aliquid eos doloremque minima repellendus similique neque corrupti!',
+    tags: ['CSS', 'JavaScript'],
+    liveUrl: 'https://github.com',
+    githubUrl: 'https://google.com',
+    image: 'https://anniebombanie.com/assets/work-personal-date-night.png',
+  },
+]
+
 const Projects = () => {
   return (
     <Container>
@@ -10,8 +29,19 @@ const Projects = () => {
           <h2 className='subtitle'>Some stuff I've built.</h2>
         </div>
         <div>
-          <Project />
-          <Project />
+          {
+            projects.map(project => (
+              <Project
+                key={project.name}
+                name={project.name}
+                description={project.description}
+                tags={project.tags}
+                liveUrl={project.liveUrl}
+                githubUrl={project.githubUrl}
+                image={project.image}
+              />
+            ))
+          }
         </div>
     </Container>
   );
