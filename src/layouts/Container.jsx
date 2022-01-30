071052664f354fdb2357cf1e970a7618ede5e870
Eduardo <96.rodedd@gmail.com>
2022-01-30 13:00:06 -0600

    some updates

--- src/layouts/Container.jsx
+++ src/layouts/Container.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
 
-const Container = ({ children, grid }) => {
+const Container = ({ children }) => {
   return (
-    <div className={`flex flex-col items-center px-8 sm:px-16 py-2 m-auto min-h-screen max-w-7xl bg-babyPowder mix-blend-darken overflow-hidden xl:overflow-visible ${grid ? 'grid grid-cols-1 content-between' : ''}`}>
+    <div className='flex flex-col items-center mx-auto px-8 sm:px-16 py-2 min-h-screen max-w-7xl bg-babyPowder mix-blend-darken overflow-hidden xl:overflow-visible'>
       <Header />
       {children}
       <Footer />
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,32 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Button } from '../components/Button';
 import { Container } from '../layouts/Container';
 
 const Home = () => {
   return (
-    <Container grid={false}>
-      <main className='relative'>
+    <Container>
+      <main className='relative my-20'>
       
-      <div className='-z-10 absolute'>
-        <div className='absolute top-0 left-0 w-[600px] h-[600px] opacity-50 rounded-full circle-1'>
+      <div className='-z-10 absolute w-full h-full'>
+        <div className='absolute -top-7 left-40 w-[500px] h-[500px] opacity-50 rounded-full circle-1'>
         </div>
-        <div className='absolute top-12 left-20 w-[600px] h-[600px] opacity-50 rounded-full circle-2'>
+        <div className='absolute -top-10 left-52 w-[500px] h-[500px] opacity-50 rounded-full circle-2'>
         </div>
       </div>
 
-      <div className='grid justify-items-center items-center grid-cols-1 gap-4 mt-8 mb-8 text-center md:mt-0 md:text-left md:grid-cols-7 md:grid-rows-3 md:justify-items-start'>
-        <p className='text-3xl md:text-4xl md:order-1 md:col-span-7 md:self-end'>Hi there 👋</p>
-        <img className='w-40 rounded-full md:order-3 md:row-span-2 md:col-span-2 md:justify-self-end md:self-center md:w-48' src="https://res.cloudinary.com/rodedd/image/upload/v1642636411/portfolio/Perfil334_dslkod.jpg" alt="Eduardo Prado" />
-        <h1 className='flex flex-col md:order-2 md:col-span-5'>
-          <span className='text-5xl md:text-5xl font-bold'>I'm <span className='text-ultraRed'>Eduardo</span></span>
-          <span className='text-3xl md:text-4xl'>Front End Developer</span> 
-        </h1>
-        <p className='paragraph text-2xl mb-6 md:order-3 md:col-span-5 md:text-3xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto iste libero quidem, aperiam, neque ipsam vitae voluptate veniam.</p>
+      <div className='grid justify-items-center items-center grid-cols-1 gap-4 mt-8 mb-8 text-center md:mt-0 md:text-left md:grid-cols-5 md:justify-items-start'>
+
+        <p className='text-3xl md:hidden '>Hi there 👋</p>
+
+        <img className='w-40 rounded-full md:order-5 md:justify-self-end md:self-center md:w-48' src="https://res.cloudinary.com/rodedd/image/upload/v1642636411/portfolio/Perfil334_dslkod.jpg" alt="Eduardo Prado" />
+        
+        <div className='space-y-3 md:order-1 md:col-span-4 md:space-y-5'>
+          <p className='hidden md:block md:text-4xl '>Hi there 👋</p>
+          <h1 className='flex flex-col'>
+            <span className='text-5xl md:text-5xl font-bold'>I'm <span className='text-ultraRed'>Eduardo</span>.</span>
+          </h1>
+          <p className='paragraph text-2xl mb-6 md:text-3xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto iste libero quidem, aperiam, neque ipsam vitae voluptate veniam.</p>
+        </div>
+
       </div>
 
       <div className='space-y-3 md:space-x-4'>
-        <Button buttonType='primary'>View my projects</Button>
-        <Button buttonType='secondary'>More about me</Button>
+        <Link to='/projects'>
+          <Button buttonType='primary'>View my projects</Button>
+        </Link>
+        <Link to='/about'>
+          <Button buttonType='secondary'>More about me</Button>
+        </Link>
       </div>
 
     </main>
