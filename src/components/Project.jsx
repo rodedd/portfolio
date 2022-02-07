4676e90c8328ec5cb1b8fe0db7c8b13246080974
Eduardo <96.rodedd@gmail.com>
2022-02-06 19:15:47 -0600

    projects updated

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,12 +3,12 @@ import { Button } from './Button';
 
 const Project = ({ name, description, tags, liveUrl, githubUrl, image, index }) => {
 
-  let par = (index + 1) % 2;
+  let mod = (index + 1) % 2;
 
   return (
     <div className='mb-20 sm:flex'>
 
-      <div className={`w-full sm:w-1/2 ${par === 0 ? 'sm:order-2' : ''}`}>
+      <div className={`w-full sm:w-1/2 ${mod === 0 ? 'sm:order-2' : ''}`}>
         <img className='mb-4' src={image} alt={name} />
       </div>
 
@@ -16,16 +16,26 @@ const Project = ({ name, description, tags, liveUrl, githubUrl, image, index }) 
         <h3 className='font-baskerville font-bold text-2xl mb-4 text-prussianBlue'>{name}</h3>
         <p className='paragraph mb-4'>{description}</p>
 
-        <span className='flex items-center mb-4'>
-          <svg className="w-6 h-6 mr-2" fill="none" stroke="#FF6978" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z" /></svg>
-          <ul className='flex text-lg text-prussianBlue'>
+        <div className='flex items-center mb-4'>
+          <svg className="w-6 h-6 mr-2 min-w-[24px]" fill="none" stroke="#FF6978" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z" /></svg>
+          <span className='flex flex-wrap text-lg text-prussianBlue'>
+            <span>
             {
               tags.map((tag, index) => (
-                <li key={tag}>{(index ? ', ' : '') + tag}</li>
+                  <span key={tag}>{(index !== tags.length - 1) ? `${tag}, ` : `${tag}.`}</span>
               ))
             }
-          </ul>
-        </span>
+            </span>
+          </span>
+          
+          {/* <ul className='flex flex-wrap text-lg text-prussianBlue'>
+            {
+              tags.map((tag, index) => (
+                  <li key={tag}>{(index !== tags.length - 1) ? `${tag}, ` : `${tag}`}</li>
+              ))
+            }
+          </ul> */}
+        </div>
 
         <div className='w-full flex flex-col sm:flex-row space-y-3 sm:space-y-0 sm:space-x-3'>
           <a href={liveUrl} target='_blank' rel='noreferrer'>
